bb8d1881aa00d80d24c8d5857ec668a0372fc07d
Jaime Ansorena <jansorenac@gmail.com>
2024-05-16 18:27:26 -0400

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
 
+// Use PORT provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/post", postRoute);
 
-app.listen(8800, () => {
+app.listen(port,"0.0.0.0", () => {
     console.log("Backend server is running!");
 })
